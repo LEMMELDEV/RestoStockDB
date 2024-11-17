a746e27d4ba42a514c70e1e58d783c216048e6fc
LEMMEL <LEMMEL@LEMMEL>
2024-11-16 21:51:11 -0500

    Creacion Clase Proveedores

---
+++ Models/Provedores.cs
@@ -0,0 +1,14 @@
+﻿namespace RestoStockDB.Models
+{
+    public class Proveedores
+    {
+        public int IdProveedor { get; set; } // Llave primaria
+        public string NombreEmpresa { get; set; }
+        public string Contacto { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+
+        // Relación uno-a-muchos con Pedido
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
