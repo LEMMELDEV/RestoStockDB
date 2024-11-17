7863516ee4a37f1cd5d805517197819d04d42ee0
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-16 21:10:08 -0500

    Se creo la clase ingredientes dentro e la carpeta models

--- Models/Ingredientes.cs
+++ Models/Ingredientes.cs
@@ -4,6 +4,11 @@ namespace RestoStockDB.Models
 {
     public class Ingrediente
     {
+            public int IdIngrediente { get; set; } // PK
+            public string Nombre { get; set; }
+            public decimal CantidadDisponible { get; set; }
+            public string UnidadMedida { get; set; }
+            public decimal PrecioUnitario { get; set; }
     }
 
 }
