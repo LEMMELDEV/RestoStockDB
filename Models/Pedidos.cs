c211ce4ae44abbeae9978a6f70b520551b1d70a6
LEMMEL <LEMMEL@LEMMEL>
2024-11-16 21:34:12 -0500

    Creacion Clase Pedidos

---
+++ Models/Pedidos.cs
@@ -0,0 +1,14 @@
+﻿namespace RestoStockDB.Models
+{
+    public class Pedidos
+    {
+        public int IdPedido { get; set; } // Llave primaria
+
+        // Llave foránea
+        public int IdProveedor { get; set; }
+        public Proveedor Proveedor { get; set; }
+
+        public DateTime FechaPedido { get; set; }
+        public decimal Total { get; set; }
+    }
+}
