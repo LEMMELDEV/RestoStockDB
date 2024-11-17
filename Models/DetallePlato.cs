32f3a68c8aa71ccd311dc0a5a6db69cb7d3c023c
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-16 21:34:13 -0500

    Creacion clase DetallePlato

---
+++ Models/DetallePlato.cs
@@ -0,0 +1,17 @@
+﻿namespace RestoStockDB.Models
+{
+    public class DetallePlato
+    {
+        public int IdDetalle { get; set; } // Llave primaria
+
+        // Llaves foráneas
+        public int IdPlato { get; set; }
+        public Plato Plato { get; set; }
+
+        public int IdIngrediente { get; set; }
+        public Ingrediente Ingrediente { get; set; }
+
+        public decimal Cantidad { get; set; }
+
+    }
+}
