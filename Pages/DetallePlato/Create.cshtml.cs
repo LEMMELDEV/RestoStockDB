7170d0e05d5247a57c1738f9c3a35f8d8a3dbfc9
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-19 00:18:53 -0500

    Create DetallesPlato

--- Pages/DetallePlato/Create.cshtml.cs
+++ Pages/DetallePlato/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestoStockDB.DATA;
+using RestoStockDB.Models;
 
 namespace RestoStockDB.Pages.DetallePlato
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly RestoStockContext _context;
+
+        public CreateModel(RestoStockContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DetallesPlato DetallesPlato { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.DetallesPlatos == null || DetallesPlato == null)
+            {
+                return Page();
+            }
+
+            _context.DetallesPlatos.Add(DetallesPlato);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
