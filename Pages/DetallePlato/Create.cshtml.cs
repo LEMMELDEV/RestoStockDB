a49a7e6866a46a876c83884d64a0f42f6a6514db
sofiaamv1105 <sofiaamv2003@gmail.com>
2024-11-18 23:48:27 -0500

    Creacion page e Index Platos

---
+++ Pages/DetallePlato/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestoStockDB.Pages.DetallePlato
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
