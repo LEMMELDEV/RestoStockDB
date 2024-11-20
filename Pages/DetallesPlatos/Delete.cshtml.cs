6ef97c6b0b519a01aa829e6aad339c5e3de54fb5
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2024-11-19 23:19:39 -0500

    actualizacion detalle platos

--- Pages/DetallesPlatos/Delete.cshtml.cs
+++ Pages/DetallesPlatos/Delete.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RestoStockDB.Pages.DetallePlato
+namespace RestoStockDB.Pages.DetallesPlatos
 {
     public class DeleteModel : PageModel
     {
